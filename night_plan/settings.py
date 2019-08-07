405c9fde5255a296af53eb1763724ff44766e684
Emmanuel Michira <emmanuelmichira@gmail.com>
2019-08-07 13:24:40 +0300

    add geodjango library to settings.py to fix deploy heroku bugs

--- night_plan/settings.py
+++ night_plan/settings.py
@@ -110,6 +110,9 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -228,3 +231,6 @@ LEAFLET_CONFIG = {
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 django_heroku.settings(locals())
+
+# GDAL_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgdal.so"
+# GEOS_LIBRARY_PATH = "/app/.heroku/vendor/lib/libgeos_c.so"
