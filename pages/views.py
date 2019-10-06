17a6eefd38c21ffe87c5e78a2f29494e2905c6fd
Emmanuel Michira <emmanuelmichira@gmail.com>
2019-10-06 05:44:49 +0300

    add query to filter events on the weekend and particular date

--- pages/views.py
+++ pages/views.py
@@ -7,21 +7,24 @@ from counties.models import County
 from categories.models import Category
 from clubs.models import Club
 from genres.models import Genre
+from datetime import datetime, date
+from django.db.models import Q
 
 def home_page(request):
     counties = County.objects.all()
     trending = Event.objects.all().order_by('-event_date').filter(event_type='1')
     featured = Event.objects.all().order_by('-event_date').filter(event_type='2')
-    tonight = Event.objects.all().order_by('-event_date').filter(event_type='3')
-    this_weekend = Event.objects.all().order_by('-event_date').filter(event_type='4')
-
+    tonight = Event.objects.filter(event_date__date=date.today())
+    this_weekend = Event.objects.filter(Q(event_date__week_day=1) | Q(event_date__week_day=7))
+    clubs = Club.objects.all().order_by('-created_at').filter(is_published=True)
 
     context = {
         "counties" : counties,
         "trending" : trending,
         "featured" : featured,
         "tonight" : tonight,
-        "this_weekend" : this_weekend
+        "weekend" : this_weekend,
+        "clubs" : clubs,
     }
     return render(request, "pages/index.html", context)
 
