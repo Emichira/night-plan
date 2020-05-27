989cb95c6c70e776a85935e4451e4435d85d251e
Emmanuel Michira <emmanuelmichira@gmail.com>
2020-05-27 21:38:17 +0300

    modify homepage for cocktails display updtae main1.css with styling modify navbar and pages/view.py

--- pages/views.py
+++ pages/views.py
@@ -12,9 +12,25 @@ from django.db.models import Q
 
 def home_page(request):
     cover_image = Event.objects.filter(is_published=True).exclude(cover_image__isnull=True).exclude(cover_image__exact='').order_by('event_date')#display all cover images in detailed category
+    clubs = Club.objects.filter(is_published=True).order_by('name')
+    happy_hour = Event.objects.filter(categories='6', is_published=True).order_by('event_date')
+    tonight = Event.objects.filter(event_date__date=date.today(), is_published=True)
+    this_weekend = Event.objects.filter(Q(event_date__week_day=1) | Q(event_date__week_day=7), is_published=True)
+    trending = Event.objects.filter(event_type='2', is_published=True).order_by('event_date')
+    genres = Genre.objects.filter(is_published=True).order_by('name')
+    categories = Category.objects.all().order_by('name')
+    counties = County.objects.all().order_by('-created_at')
 
     context = {
         "covers" : cover_image,
+        "clubs" : clubs,
+        "happy_hour" : happy_hour,
+        "tonight" : tonight,
+        "this_weekend" : this_weekend,
+        "trending" : trending,
+        'genres' : genres,
+        "categories" : categories,
+        "counties" : counties,
     }
     return render(request, "pages/index.html", context)
 
